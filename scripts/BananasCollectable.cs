3bdfd305651658fa812fe0bc8ba2e170862b1f4b
DuckyDay-0 <duckyday864@gmail.com>
2024-11-09 01:33:45 +0200

    Added collected animation after getting the banana in level 2 ,added content to level 3
    Connected animation_finished signal to the node so we can detect when the idle banana animation have finished and then start the collected animation

--- scripts/BananasCollectable.cs
+++ scripts/BananasCollectable.cs
@@ -4,20 +4,40 @@ using System;
 public partial class BananasCollectable : Area2D
 {
 	Levelmanager levelManager;
+	AnimatedSprite2D animatedSprite;
+	bool isBananaCollected = false;
+
 	public override void _Ready()
 	{
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
 		levelManager = GetNode<Levelmanager>("%Levelmanager");
-	}
+		animatedSprite = GetNode<AnimatedSprite2D>("bananaAnimation");
 
-	public void OnBodyEntered(Node body)
+		//connect the animation_finished signal so we can detect when the prev animation have finished
+        animatedSprite.Connect("animation_finished", new Callable(this, nameof(OnAnimationFinished)));
+
+
+    }
+
+    public void OnBodyEntered(Node body)
 	{
 		if (body.Name == "player")
 		{
 			GD.Print("Getting Banana");
 			levelManager.GetBanana();
-			QueueFree();
-		}
+			isBananaCollected = true;
+			animatedSprite.Play("collected");			
+        }
 	}
 
+	public void OnAnimationFinished()
+	{
+		//If banana is collected delete the node and reset the the IsBananaCollected
+		if (isBananaCollected == true)
+		{
+			GD.Print("Collected banana");
+			QueueFree();
+			isBananaCollected = false;
+		}
+	}
 }
