b94c8ee489fa123f7ce826b78c89711ba5b40255
DuckyDay-0 <duckyday864@gmail.com>
2025-01-01 22:17:53 +0200

    Added export for the fruits animation ,so no need for separate scenes for the separate fruits
    just code added
    HasAnimation() method added
    In _Ready method we are pre-selecting the animation depending on the Exported var

--- scripts/BananasCollectable.cs
+++ scripts/BananasCollectable.cs
@@ -3,41 +3,66 @@ using System;
 
 public partial class BananasCollectable : Area2D
 {
+	[Export]
+	string animationName = "";
+
 	Levelmanager levelManager;
 	AnimatedSprite2D animatedSprite;
-	bool isBananaCollected = false;
+	bool isFruitCollected = false;
 
 	public override void _Ready()
 	{
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
 		levelManager = GetNode<Levelmanager>("%Levelmanager");
-		animatedSprite = GetNode<AnimatedSprite2D>("bananaAnimation");
+		animatedSprite = GetNode<AnimatedSprite2D>("fruitAnimation");
 
 		//connect the animation_finished signal so we can detect when the prev animation have finished
         animatedSprite.Connect("animation_finished", new Callable(this, nameof(OnAnimationFinished)));
 
-
+		if (HasAnimation(animationName))
+		{
+			animatedSprite.Animation = animationName;
+			animatedSprite.Play(animationName);
+		}
     }
+
+	 
+
+	public bool HasAnimation(string animationName)
+	{
+		var animations = animatedSprite.SpriteFrames.GetAnimationNames();
+
+		foreach (var anim in animations)
+		{
+			if (anim == animationName)
+			{
+				return true;
+			}
+			
+		}
+
+		return false;
+	}
 
     public void OnBodyEntered(Node body)
 	{
 		if (body.Name == "player")
 		{
-			GD.Print("Getting Banana");
+			GD.Print("Getting Fruit");
 			levelManager.GetBanana();
-			isBananaCollected = true;
+            isFruitCollected = true;
 			animatedSprite.Play("collected");			
         }
 	}
 
 	public void OnAnimationFinished()
 	{
-		//If banana is collected delete the node and reset the the IsBananaCollected
-		if (isBananaCollected == true)
+		//If the fruit is collected delete the node and reset the IsBananaCollected
+		if (isFruitCollected == true)
 		{
-			GD.Print("Collected banana");
+			GD.Print("Collected fruit");
 			QueueFree();
-			isBananaCollected = false;
+            isFruitCollected = false;
 		}
 	}
 }
