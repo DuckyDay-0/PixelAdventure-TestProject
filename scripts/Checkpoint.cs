a2adc89a6d02430965f2644ad5ebcc1332709cb9
DuckyDay-0 <duckyday864@gmail.com>
2025-01-04 02:06:06 +0200

    Added animations to the checkpoint sprite
    Added noFlag when the player haven't activated the checkpoint
    Added flagOut animation when the player enters the checkpoint area
    Added flagIdle animation when the checkpoint is active

--- scripts/Checkpoint.cs
+++ scripts/Checkpoint.cs
@@ -3,9 +3,19 @@ using System;
 
 public partial class Checkpoint : Area2D
 {
+
+    AnimatedSprite2D animatedSprite;
+    bool isCheckpointActive = false;
     public override void _Ready()
     {
+
         Connect("body_entered", new Callable(this, nameof(onCheckpointBodyEntered)));
+
+        animatedSprite = GetNode<AnimatedSprite2D>("flagAnimation");
+        animatedSprite.Connect("animation_finished", new Callable(this, nameof(onAnimationFinished)));
+
+        animatedSprite.Play("checkpointNoFlag");
+        GD.Print("noFlag");
     }
 
     private void onCheckpointBodyEntered(Node body)
@@ -14,6 +24,24 @@ public partial class Checkpoint : Area2D
         {
             player.SetRespawnPosition(GlobalPosition);
             GD.Print("Checkpoint mate");
+
+            isCheckpointActive = true;
+            animatedSprite.Play("checkpointFlagOut");
+            GD.Print("Flag Out");
+        }
+     
+
+    }
+
+    private void onAnimationFinished()
+    {
+        GD.Print("onAnimation");
+
+        if (isCheckpointActive)
+        {
+            animatedSprite.Play("checkpointFlagIdle", -1, true);
+            GD.Print("FlagIdle");
+
         }
     }
 }
